3b0129884857c6d4baa03a4ee13f8b49ecadeca8
SilviaCostaCookier <danielmarcelino549@gmail.com>
2026-06-26 17:17:07 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,8 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Suas credenciais exclusivas do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBON-cfEpnuQf496m9pnZJW24XoR_2nlwc",
   authDomain: "vide-digital-saas.firebaseapp.com",
@@ -12,6 +11,9 @@ const firebaseConfig = {
   appId: "1:891590456336:web:bd51ac50399465b886c695"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exporta os serviços para usar nos outros arquivos .js
 export const auth = getAuth(app);
 export const db = getFirestore(app);
