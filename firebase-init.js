fd63cfc5870fe3e23ee9484858996db6ce926325
SilviaCostaCookier <danielmarcelino549@gmail.com>
2026-06-29 16:19:19 -0300

    Upgrade Firebase SDK to version 10.12.0

--- firebase-init.js
+++ firebase-init.js
@@ -1,16 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+// Dados extraídos diretamente do seu console oficial (vide-digital-saas)
 const firebaseConfig = {
-  apiKey: "AIzaSyBON-cfEpnuQf496m9pnZJW24XoR_2nlwc",
-  authDomain: "vide-digital-saas.firebaseapp.com",
-  projectId: "vide-digital-saas",
-  storageBucket: "vide-digital-saas.firebasestorage.app",
-  messagingSenderId: "891590456336",
-  appId: "1:891590456336:web:bd51ac50399465b886c695"
+    apiKey: "AIzaSyBON-cfEpnuQf496m9pnZJW24XoR_2nlwc",
+    authDomain: "vide-digital-saas.firebaseapp.com",
+    projectId: "vide-digital-saas",
+    storageBucket: "vide-digital-saas.firebasestorage.app",
+    messagingSenderId: "891590456336",
+    appId: "1:891590456336:web:bd51ac50399465b886c695"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
